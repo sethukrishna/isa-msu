b8ce37831eaa69dabe2eded35c30f707a5993679
jaihind <plnu1@murraystate.edu>
2015-12-23 12:44:35 +0000

    donw with cms setting up

---
+++ isa-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsaTest::Application.config.secret_token = '8d24f940c02b4a8173bde7e16583331eeed3aea3065ac2a3d5fed3b7c0895df2bf55e522dc7555776f0ef5794cfd3dff26fc4eeabecb3aa48c27a76cb2090466'
---
+++ isa/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isa::Application.config.secret_token = '3f4bf3b5658d6b2b74c8fdc9c700e9a4d598778a89399165f6eda43d6332e21e7ae6f0e4a34e41f72fbabb149adf8aa8ee5929f9ab30e0a44b3509cf494f1b1b'
